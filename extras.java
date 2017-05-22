c822a9c5d6a79c5c05272b61264876d39f77006e
AlfredoAlvarez21 <Alfredo.Alvarez@gompersstudent.org>
2017-05-22 15:02:37 -0700

    Add files via upload

---
+++ extras.java
@@ -0,0 +1,34 @@
+package methods;
+
+public class extras {
+		public static void printMilk(int number){			
+			
+			
+			
+		
+		
+System.out.println(number + " bottles on the wall. Take one down, and pass it around. "+ number + " bottles on the wall. ");
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+		
+		public static void main(String[] args) {
+		for (int bottles = 99; bottles >= 0; bottles--) {
+			printMilk(bottles);
+		}	
+		}
+	}
+
